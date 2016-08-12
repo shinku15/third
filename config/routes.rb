ceb38a908fa7c1b103a4a2b9b673037489b91d14
shinku <shrinkhala@classpro.in>
2016-08-12 14:51:03 +0530

    Finish user  signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   
   root "application#hello"
 
-   resources :static_pages
+   
    get '/home' => 'static_pages#home'
    get '/help'	=>	'static_pages#help'
    get '/about'	=> 'static_pages#about'
+   resources :users
    
 
    get '/signup' =>'users#new'
